60508ecd4822dcbc48332c512eb268e998d6897d
Somnathumapathi <somnathumapathi7@gmail.com>
2024-07-15 02:31:36 +0530

    stream

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -7,6 +7,14 @@ import 'package:http/http.dart' as http;
 import 'package:jailerecord/constants/utils.dart';
 import 'package:jailerecord/features/auth/screens/signup.dart';
 import 'package:jailerecord/features/auth/screens/signin.dart';
+import 'package:jailerecord/features/lawyer/screens/lawyerHome.dart';
+import 'package:jailerecord/features/police/screens/policeHome.dart';
+import 'package:jailerecord/models/lawyer.dart';
+import 'package:jailerecord/models/police.dart';
+import 'package:jailerecord/provider/laywerProvider.dart';
+import 'package:jailerecord/provider/policeProvider.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   static void signUp(
@@ -34,40 +42,60 @@ class AuthService {
           headers: <String, String>{
             'Content-Type': 'application/json; charset=UTF-8'
           });
-
-      print('$uri/addNewUser');
-      print('qenrojf');
       httpHandler(
           res: res,
           context: context,
           onSuccess: () {
-            print('hbohqb');
             showSnackBar(context, 'User created now login');
             Navigator.push(context,
                 MaterialPageRoute(builder: (context) => SigninScreen()));
           });
     } catch (e) {
-      print('erreeeeeeeeeeeeeerrrrrrrr');
       showSnackBar(context, e.toString());
     }
   }
 
-  static void signin(
-      {required BuildContext context,
-      required String email,
-      required String password,
-      required String role}) async {
+  static void signin({
+    required BuildContext context,
+    required String email,
+    required String password,
+  }) async {
     try {
       if (email.isNotEmpty && password.isNotEmpty) {
         final UserCredential userCred = await FirebaseAuth.instance
             .signInWithEmailAndPassword(email: email, password: password);
         final uid = userCred.user!.uid;
+        http.Response res = await http.get(Uri.parse('$uri/getUser?uid= $uid'),
+            headers: <String, String>{
+              'Content-Type': 'application/json; charset=UTF-8'
+            });
+        httpHandler(
+            res: res,
+            context: context,
+            onSuccess: () async {
+              print(res.body);
+              final prefs = await SharedPreferences.getInstance();
+              await prefs.setString('uid', uid);
+              if (jsonDecode(res.body)['role'] == 'police') {
+                final police = Police.fromMap(jsonDecode(res.body)['data']);
+                Provider.of<PoliceProvider>(context, listen: false)
+                    .setPolice(police);
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => PoliceHome()));
+              } else if (jsonDecode(res.body)['role'] == 'lawyer') {
+                final lawyer = Lawyer.fromMap(jsonDecode(res.body)['data']);
+                Provider.of<LawyerProvider>(context, listen: false)
+                    .setLawyer(lawyer);
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LawyerHomePage()));
+              }
+
+              showSnackBar(context, 'Successful');
+            });
       }
     } catch (e) {
       showSnackBar(context, e.toString());
     }
-    // if (role == 'police') {
-    // } else if (role == 'laywer') {}
   }
 
   static void signOut() {
---
+++ lib/features/lawyer/screens/lawyerHome.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LawyerHomePage extends StatefulWidget {
+  const LawyerHomePage({super.key});
+
+  @override
+  State<LawyerHomePage> createState() => _LawyerHomePageState();
+}
+
+class _LawyerHomePageState extends State<LawyerHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

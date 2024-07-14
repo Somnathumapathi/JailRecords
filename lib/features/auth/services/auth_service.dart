0b1d7b8402d84fc73231f233d5c080e200a0042a
Somnathumapathi <somnathumapathi7@gmail.com>
2024-07-15 01:35:15 +0530

    auth

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:jailerecord/constants/globalvariables.dart';
+import 'package:http/http.dart' as http;
+import 'package:jailerecord/constants/utils.dart';
+import 'package:jailerecord/features/auth/screens/signin.dart';
 
 class AuthService {
   static void signUp(
@@ -7,15 +13,36 @@ class AuthService {
       required String name,
       required String email,
       required String password,
+      String? contact,
+      String? badge,
       required String role}) async {
     try {
       final UserCredential userCred = await FirebaseAuth.instance
           .createUserWithEmailAndPassword(email: email, password: password);
       final uid = userCred.user!.uid;
-      // if (role == 'police') {
-      // } else if (role == 'laywer') {}
+      http.Response res = await http.post(Uri.parse('$uri/api/signup'),
+          body: jsonEncode({
+            "name": name,
+            "role": role,
+            'emailId': email,
+            'uid': uid,
+            "badge": badge,
+            "contact": contact
+          }),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8'
+          });
+      httpHandler(
+          res: res,
+          context: context,
+          onSuccess: () {
+            print('hbohqb');
+            showSnackBar(context, 'User created now login');
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SigninScreen()));
+          });
     } catch (e) {
-      print(e);
+      showSnackBar(context, e.toString());
     }
   }
 
